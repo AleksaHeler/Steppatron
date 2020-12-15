3c32224a89886c23110b367e69cbda6e251c5c0b
TrAceX <djole1271952@gmail.com>
2020-12-15 10:30:00 +0100

    Merge playNote.c -> steppatron.c

--- src/steppatron.c
+++ src/steppatron.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Output file name
+#define FILE_NAME "testFile.txt"
+
 // RawMidi ALSA hardware port to be used
 #define MIDI_PORT "hw:1,0,0"
 
@@ -283,6 +286,13 @@ int readUsbMessage(midiMessage_t* message, snd_rawmidi_t* device) {
 // 1. - u for USB, k for keyboard, f for file
 // 2. - filename
 int main(int argc, char **argv) {
+    int file_desc = open(FILE_NAME, O_RDWR);
+
+    if (file_desc < 0) {
+        printf("Error, %s not opened\n", FILE_NAME);
+        return -1;
+    }
+
     if (argc == 1 || strcmp(argv[1], "u") == 0) {
         // Read from USB
         snd_rawmidi_t *midiIn;
@@ -330,7 +340,77 @@ int main(int argc, char **argv) {
 
     } else if (strcmp(argv[1], "k") == 0) {
         // Read from keyboard
+        int ret_val;
+        char input;
+        int octave;
 
+        printf("Choose octave [1-7]: ");
+        scanf("%d", &octave);
+        getch(); //da pokupi enter
+
+        printf("Start playing notes\n");
+
+        while (1) {
+            input = getch();
+
+            switch (input) {
+            //donji red
+            case 'a':
+                input = (char)C[octave].MIDINumber;
+                break;
+            case 's':
+                input = (char)D[octave].MIDINumber;
+                break;
+            case 'd':
+                input = (char)E[octave].MIDINumber;
+                break;
+            case 'f':
+                input = (char)F[octave].MIDINumber;
+                break;
+            case 'g':
+                input = (char)G[octave].MIDINumber;
+                break;
+            case 'h':
+                input = (char)A[octave].MIDINumber;
+                break;
+            case 'j':
+                input = (char)B[octave].MIDINumber;
+                break;
+
+            //gornji red
+            case 'w':
+                input = (char)M[5 * (octave - 1)].MIDINumber;
+                break;
+            case 'e':
+                input = (char)M[5 * (octave - 1) + 1].MIDINumber;
+                break;
+            case 't':
+                input = (char)M[5 * (octave - 1) + 2].MIDINumber;
+                break;
+            case 'y':
+                input = (char)M[5 * (octave - 1) + 3].MIDINumber;
+                break;
+            case 'u':
+                input = (char)M[5 * (octave - 1) + 4].MIDINumber;
+                break;
+
+            default:
+                continue;
+            }
+
+            if (input == 'q') {
+                close(file_desc);
+                break;
+            }
+
+            ret_val = write(file_desc, &input, 1);
+
+            if (ret_val == 0) {
+                printf("Error writing to file\n");
+                close(file_desc);
+                return 2;
+            }
+        }
 
     } else {
         printf("Invalid arguments!\n");
